ee51236f6102551e87eaef55613edc4f484386ea
yakshmaishery <yakshmaishery@users.noreply.github.com>
2025-03-08 23:34:30 +0530

    Changed the width of drawer

--- src/lib/Components/NavBar.svelte
+++ src/lib/Components/NavBar.svelte
@@ -16,7 +16,7 @@
       // Provide your property overrides:
       // bgDrawer: 'bg-purple-900 text-white',
       // bgBackdrop: 'bg-gradient-to-tr from-indigo-500/50 via-purple-500/50 to-pink-500/50',
-      width: 'w-[280px] md:w-[480px]',
+      width: 'w-[100%] md:w-[100%]',
       padding: 'p-4',
       rounded: 'rounded-xl',
    };
