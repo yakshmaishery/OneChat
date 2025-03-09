6d9bbb31f675fe05ccc35170af72d621eb40dc49
yakshmaishery <yakshmaishery@users.noreply.github.com>
2025-03-09 11:33:02 +0530

    Bubble window UI change

--- src/lib/Components/ChatWindow.svelte
+++ src/lib/Components/ChatWindow.svelte
@@ -10,7 +10,7 @@
          <div class="grid grid-cols-[auto_80%] gap-2">
             <!-- <Avatar src="https://i.pravatar.cc/?img={bubble.avatar}" width="w-12" /> -->
             <div></div>
-            <div class="card p-4 variant-soft rounded-tl-none space-y-2">
+            <div class="card p-4 variant-soft rounded-tr-none space-y-2">
                <header class="flex justify-between items-center">
                   <p class="font-bold"><button class="btn" style="padding: 0;" on:click={()=>{navigator.clipboard.writeText(item.message)}}>📋</button></p>
                   <small class="opacity-50">{item.timestamp.toLocaleDateString()} {item.timestamp.toLocaleTimeString()}</small>
@@ -21,7 +21,7 @@
       {/if}
       {#if item.type!="Sender"}
          <div class="grid grid-cols-[80%_auto] gap-2">
-            <div class="card p-4 rounded-tr-none space-y-2 ">
+            <div class="card p-4 rounded-tl-none space-y-2 ">
                <header class="flex justify-between items-center">
                   <p class="font-bold"><button class="btn" style="padding: 0;" on:click={()=>{navigator.clipboard.writeText(item.message)}}>📋</button></p>
                   <small class="opacity-50">{item.timestamp.toLocaleDateString()} {item.timestamp.toLocaleTimeString()}</small>
